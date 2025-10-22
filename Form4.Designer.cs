42b6cecd4a19c95594a0e7a180196eb6ca9a26b0
creater <115034448+Ruslana2005@users.noreply.github.com>
2025-10-22 13:21:29 +0300

    Add files via upload

---
+++ Form4.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace CompGraphicsLab05
+{
+    partial class Form4
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.CheckBox checkBox2;
+
+
+private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.GroupBox groupBoxMode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.groupBoxMode = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.leftPanel.SuspendLayout();
+            this.groupBoxMode.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.leftPanel.Controls.Add(this.groupBoxMode);
+            this.leftPanel.Controls.Add(this.checkBox2);
+            this.leftPanel.Controls.Add(this.button2);
+            this.leftPanel.Controls.Add(this.button1);
+            this.leftPanel.Controls.Add(this.label1);
+            this.leftPanel.Controls.Add(this.numericUpDown1);
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.ForeColor = System.Drawing.Color.White;
+            this.leftPanel.Location = new System.Drawing.Point(0, 0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Padding = new System.Windows.Forms.Padding(8);
+            this.leftPanel.Size = new System.Drawing.Size(180, 480);
+            this.leftPanel.TabIndex = 0;
+            // 
+            // groupBoxMode
+            // 
+            this.groupBoxMode.Controls.Add(this.radioButton3);
+            this.groupBoxMode.Controls.Add(this.radioButton2);
+            this.groupBoxMode.Controls.Add(this.radioButton1);
+            this.groupBoxMode.ForeColor = System.Drawing.Color.White;
+            this.groupBoxMode.Location = new System.Drawing.Point(11, 12);
+            this.groupBoxMode.Name = "groupBoxMode";
+            this.groupBoxMode.Size = new System.Drawing.Size(158, 110);
+            this.groupBoxMode.TabIndex = 0;
+            this.groupBoxMode.TabStop = false;
+            this.groupBoxMode.Text = "Режим";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Location = new System.Drawing.Point(12, 72);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(130, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Перемещение";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(12, 46);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(130, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Удаление";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            //
+
+
+this.radioButton1.Location = new System.Drawing.Point(12, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(130, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Добавление";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Location = new System.Drawing.Point(14, 132);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(150, 22);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Показать полилинию";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button2 (Clear)
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(14, 420);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1 (Back)
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(14, 452);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1 (marker size)
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(14, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Размер метки точки:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(16, 190);
+            this.numericUpDown1.Minimum = new decimal(new int[] { 6, 0, 0, 0 });
+            this.numericUpDown1.Maximum = new decimal(new int[] { 42, 0, 0, 0 });
+            this.numericUpDown1.Value = new decimal(new int[] { 14, 0, 0, 0 });
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(180, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(620, 480);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+
+
+this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // Form4 (this)
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.leftPanel);
+            this.MinimumSize = new System.Drawing.Size(640, 360);
+            this.Name = "Form4";
+            this.Text = "Bezier Editor — визуально изменено";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form4_FormClosing);
+            this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
+            this.groupBoxMode.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CompGraphicsLab05
+{
+    public partial class Form4 : Form
+    {
+        private Form1 _form1;
+        private List<PointF> points;
+        private Bitmap bmp;
+
+        private PointF additionalPoint;
+        private int index_of_moving_point; // индекс точки, которую будем передвигать
+
+        public Form4(Form1 form1)
+        {
+            _form1 = form1;
+            InitializeComponent();
+            points = new List<PointF>();
+            radioButton1.Checked = true; // режим добавления по умолчанию
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            index_of_moving_point = -1;
+            checkBox2.Checked = true; // показывать полилинию по умолчанию
+            additionalPoint = new PointF();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // возврат на предыдущую форму
+            this.Visible = false;
+            _form1.Visible = true;
+        }
+
+        private void Form4_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Очистка 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            radioButton1.Checked = true;
+            checkBox2.Checked = true;
+            index_of_moving_point = -1;
+            additionalPoint = new PointF();
+            DrawElements();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked) // добавление точки
+            {
+                points.Add(e.Location);
+            }
+            else if (radioButton2.Checked) // удаление точки
+            {
+                DeletePoint(e.Location.X, e.Location.Y);
+            }
+            DrawElements();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                index_of_moving_point = points.FindIndex(el => (el.X > e.X - 6) && (el.X < e.X + 6) && (el.Y > e.Y - 6) && (el.Y < e.Y + 6));
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                if (index_of_moving_point >= 0)
+                {
+                    points[index_of_moving_point] = new PointF(e.X, e.Y);
+                    // удаляем возможную временную точку
+                    DeletePoint(additionalPoint.X, additionalPoint.Y);
+                    DrawElements();
+                    index_of_moving_point = -1;
+                }
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // во время перетаскивания обновляем положение точки "вживую"
+            if (radioButton3.Checked && index_of_moving_point >= 0)
+            {
+                points[index_of_moving_point] = new PointF(e.X, e.Y);
+                DrawElements();
+            }
+            else
+            {
+                // можно подсвечивать точку при наведении
+                int hover = points.FindIndex(el => (el.X > e.X - 6) && (el.X < e.X + 6) && (el.Y > e.Y - 6) && (el.Y < e.Y + 6));
+                if (hover >= 0)
+
+
+{
+                    pictureBox1.Cursor = Cursors.Hand;
+                }
+                else
+                {
+                    pictureBox1.Cursor = Cursors.Cross;
+                }
+            }
+        }
+
+        // Удаление точки (по близости)
+        private void DeletePoint(float x, float y)
+        {
+            int index_for_delete = points.FindIndex(el => (el.X > x - 6) && (el.X < x + 6) && (el.Y > y - 6) && (el.Y < y + 6));
+            if (index_for_delete >= 0)
+            {
+                points.RemoveAt(index_for_delete);
+                bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = bmp;
+                DrawElements();
+            }
+        }
+
+        // Отрисовка всех элементов — фон, сетка, полилиния опорных точек, сами точки, и кривые Безье
+        private void DrawElements()
+        {
+            if (bmp == null)
+                bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                // 1) Фон — вертикальный градиент
+                using (LinearGradientBrush br = new LinearGradientBrush(pictureBox1.ClientRectangle, Color.FromArgb(30, 40, 60), Color.FromArgb(10, 15, 25), 90f))
+                {
+                    g.FillRectangle(br, pictureBox1.ClientRectangle);
+                }
+
+                // 2) Сетка
+                int grid = 25;
+                using (Pen gridPen = new Pen(Color.FromArgb(30, 255, 255, 255)))
+                {
+                    for (int x = 0; x < pictureBox1.Width; x += grid)
+                        g.DrawLine(gridPen, x, 0, x, pictureBox1.Height);
+                    for (int y = 0; y < pictureBox1.Height; y += grid)
+                        g.DrawLine(gridPen, 0, y, pictureBox1.Width, y);
+                }
+
+                // 3) Полилиния между опорными точками (пунктиром) — если опция включена
+                if (checkBox2.Checked && points.Count > 1)
+                {
+                    using (Pen polyPen = new Pen(Color.FromArgb(180, 200, 200, 255), 1.5f))
+                    {
+                        polyPen.DashStyle = DashStyle.Dash;
+                        g.DrawLines(polyPen, points.ToArray());
+                    }
+                }
+
+                // 4) Кривые Безье (кусочно из каждых 4 последовательных точек)
+                if (points.Count >= 4)
+                {
+                    // рисуем насыщенную цветную кривую
+                    // пройдемся по последовательным блокам из 4 точек (0..3,1..4,2..5,...)
+                    for (int start = 0; start <= points.Count - 4; start += 1)
+                    {
+                        List<PointF> bezierPts = new List<PointF>();
+                        // нарисуем сглаженную кривую путем вычисления точек по формуле кубического Безье
+                        for (float t = 0; t <= 1.0001f; t += 0.01f)
+                        {
+                            PointF p = EvalCubicBezier(points[start], points[start + 1], points[start + 2], points[start + 3], t);
+                            bezierPts.Add(p);
+                        }
+
+                        // градиентный пер для кривой (изменение цвета по длине)
+                        using (Pen bezPen = new Pen(Color.FromArgb(220, 255, 140, 80), 3.5f))
+                        {
+                            bezPen.LineJoin = LineJoin.Round;
+                            if (bezierPts.Count > 1)
+                                g.DrawLines(bezPen, bezierPts.ToArray());
+                        }
+
+                        // небольшая "сияющая" обводка
+                        using (Pen glow = new Pen(Color.FromArgb(60, 255, 200, 120), 8f))
+                        {
+                            glow.LineJoin = LineJoin.Round;
+                            if (bezierPts.Count > 1)
+                                g.DrawLines(glow, bezierPts.ToArray());
+                        }
+                    }
+                }
+
+
+// 5) Рисуем опорные точки — крупные закрашенные маркеры с номером
+                for (int i = 0; i < points.Count; i++)
+                {
+                    PointF p = points[i];
+                    float size = (float)numericUpDown1.Value; // размер точки можно регулировать
+                    RectangleF rect = new RectangleF(p.X - size / 2f, p.Y - size / 2f, size, size);
+
+                    // тень/свечение
+                    using (SolidBrush shadow = new SolidBrush(Color.FromArgb(80, 0, 0, 0)))
+                    {
+                        g.FillEllipse(shadow, rect.X + 2, rect.Y + 2, rect.Width, rect.Height);
+                    }
+
+                    // основной заполненный круг
+                    using (LinearGradientBrush fill = new LinearGradientBrush(rect, Color.FromArgb(255, 255, 190, 100), Color.FromArgb(255, 200, 90, 30), 45f))
+                    {
+                        g.FillEllipse(fill, rect);
+                    }
+
+                    // обводка
+                    using (Pen border = new Pen(Color.FromArgb(220, 60, 30, 0), 2f))
+                    {
+                        g.DrawEllipse(border, rect.X, rect.Y, rect.Width, rect.Height);
+                    }
+
+                    // номер точки
+                    using (SolidBrush txtBrush = new SolidBrush(Color.White))
+                    using (Font f = new Font("Segoe UI", 8f, FontStyle.Bold))
+                    {
+                        string idx = i.ToString();
+                        SizeF ts = g.MeasureString(idx, f);
+                        g.DrawString(idx, f, txtBrush, p.X - ts.Width / 2f, p.Y - ts.Height / 2f - size / 2f - 2);
+                    }
+                }
+
+                // 6) при режиме перемещения подсвечиваем выбранную точку (если есть)
+                if (index_of_moving_point >= 0 && index_of_moving_point < points.Count)
+                {
+                    PointF p = points[index_of_moving_point];
+                    using (Pen sel = new Pen(Color.Lime, 2f))
+                    {
+                        float s = (float)numericUpDown1.Value + 6;
+                        g.DrawEllipse(sel, p.X - s / 2f, p.Y - s / 2f, s, s);
+                    }
+                }
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private PointF EvalCubicBezier(PointF p0, PointF p1, PointF p2, PointF p3, float t)
+        {
+            // стандартная формула кубического Безье
+            float u = 1 - t;
+            float tt = t * t;
+            float uu = u * u;
+            float uuu = uu * u;
+            float ttt = tt * t;
+
+            float x = uuu * p0.X;
+            x += 3 * uu * t * p1.X;
+            x += 3 * u * tt * p2.X;
+            x += ttt * p3.X;
+
+            float y = uuu * p0.Y;
+            y += 3 * uu * t * p1.Y;
+            y += 3 * u * tt * p2.Y;
+            y += ttt * p3.Y;
+
+            return new PointF(x, y);
+        }
+
+        // при изменении размеров контрольного поля пересоздаем bitmap
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            if (pictureBox1.Width > 0 && pictureBox1.Height > 0)
+            {
+                bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = bmp;
+                DrawElements();
+            }
+        }
+
+        // изменение значения размера маркера
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            DrawElements();
+        }
+    }
+}
